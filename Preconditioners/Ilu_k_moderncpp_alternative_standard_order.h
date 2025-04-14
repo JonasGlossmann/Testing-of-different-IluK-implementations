bde19cf93e02a353f6bd879c1db0942087dcef01
DESKTOP-MDU8E1M\jonas <uudxv@student.kit.edu>
2025-04-14 13:38:45 +0200

    Put alternative implementation into separate class

--- Preconditioners/Ilu_k_moderncpp_alternative_standard_order.h
+++ Preconditioners/Ilu_k_moderncpp_alternative_standard_order.h
@@ -1,13 +1,13 @@
 //
-// Created by jonas on 08.04.2025.
+// Created by jonas on 14.04.2025.
 //
 
-#ifndef ILUK_MODERNCPP_STANDARD_ORDER_H
-#define ILUK_MODERNCPP_STANDARD_ORDER_H
+#ifndef ILU_K_MODERNCPP_ALTERNATIVE_STANDARD_ORDER_H
+#define ILU_K_MODERNCPP_ALTERNATIVE_STANDARD_ORDER_H
+
 #include "Preconditioner.h"
 #include "../misc/data_structs.h"
 #include "../Matrix Management/COOMatrixSorted.h"
-
 
 void sparse_row_subtraction_with_level (std::vector<MatrixEntry>& x,const std::vector<MatrixEntry>& y,
      std::vector<int>& x_lvl, const std::vector<int>& y_lvl, double alpha, int alpha_lvl);
@@ -15,15 +15,14 @@ void sparse_row_subtraction_with_level (std::vector<MatrixEntry>& x,const std::v
 void filter_entries(std::vector<MatrixEntry>& vector,std::vector<int>& lvl, int max_lvl);
 
 
-class Iluk_moderncpp_standard_order final: public Preconditioner {
+class Ilu_k_moderncpp_alternative_standard_order final: public Preconditioner {
 public:
     std::vector<MatrixEntry> L;
     std::vector<MatrixEntry> U;
 
-    Iluk_moderncpp_standard_order(COOMatrixSorted A_ext, int level);
+    Ilu_k_moderncpp_alternative_standard_order(COOMatrixSorted A_ext, int level);
     DenseVector apply(DenseVector residual) override;
 };
 
 
-
-#endif //ILUK_MODERNCPP_STANDARD_ORDER_H
+#endif //ILU_K_MODERNCPP_ALTERNATIVE_STANDARD_ORDER_H
